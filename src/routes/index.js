af7fe15d6eae182299ae801afd422aff31dd3a92
Tien Nguyen <tienhype@gmail.com>
2023-07-12 19:15:32 +0700

    Xây dựng UI phần Sidebar (#1)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import HeaderOnly from '~/layouts/HeaderOnly';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Live from '~/pages/Live';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
@@ -9,6 +10,7 @@ import config from '~/config';
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
